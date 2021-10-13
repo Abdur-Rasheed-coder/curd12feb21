918fac39c206d2d585e867123bef1ec984ee0e0f
Abdul <abdul@gmail.com>
2021-10-13 21:32:51 +0530

    abdul commit2

--- src/main/java/curd/example/curd12feb21/api/rest/AdminController.java
+++ src/main/java/curd/example/curd12feb21/api/rest/AdminController.java
@@ -1,6 +1,6 @@
 package curd.example.curd12feb21.api.rest;
 
-import java.util.List1;
+import java.util.List11;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
